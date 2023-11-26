213ca85e1c6b4ab254fa3abf0f2119124a108e2b
Hozaneybo <maheyb01@easv365.dk>
2023-11-26 19:50:09 +0100

    conitine lesson inplementation

--- Infrastructure/Interfaces/ILessonRepository.cs
+++ Infrastructure/Interfaces/ILessonRepository.cs
@@ -6,7 +6,8 @@ public interface ILessonRepository
 {
     Task<IEnumerable<Lesson>> GetAllLessons();
     Task<Lesson> GetLessonById(int id);
-    Task<Lesson> AddLesson(string title, string content, int courseId, ICollection<LessonPicture> pictureUrls, ICollection<LessonVideo> videoUrls);
+    Task<Lesson> AddLesson(string title, string content, int courseId,  IEnumerable<string> pictureUrls, IEnumerable<string> videoUrls);
     Task<Lesson> UpdateLesson(int id, string title, string content, int courseId, IEnumerable<string> pictureUrls, IEnumerable<string> videoUrls);
     Task DeleteLesson(int id);
+    
 }
--- Service/LessonService.cs
+++ Service/LessonService.cs
@@ -2,6 +2,7 @@
 using Infrastructure.Models;
 
 
+
 namespace Service
 {
     public class LessonService
@@ -23,10 +24,24 @@ namespace Service
         {
             return _lessonRepository.GetLessonById(id).Result;
         }
-
-        public Lesson AddLesson(string title, string content, int courseId, ICollection<LessonPicture> pictureUrls, ICollection<LessonVideo> videoUrls)
+        
+        
+        /*public async Task<Lesson> AddLesson(LessonDto lessonDto)
         {
-            return _lessonRepository.AddLesson(title, content, courseId, pictureUrls, videoUrls).Result;
+            var pictureUrls = lessonDto.ImgUrls?.Select(p => p.ImgUrl) ?? Enumerable.Empty<string>();
+            var videoUrls = lessonDto.VideoUrls?.Select(v => v.VideoUrl) ?? Enumerable.Empty<string>();
+
+            return await _lessonRepository.AddLesson(lessonDto.Title, lessonDto.Content, lessonDto.CourseId, pictureUrls, videoUrls);
+        }*/
+
+        
+        
+        public async Task<Lesson> AddLesson(Lesson lessonDto)
+        {
+            var pictureUrls = lessonDto.ImgUrls?.Select(p => p.ImgUrl) ?? Enumerable.Empty<string>();
+            var videoUrls = lessonDto.VideoUrls?.Select(v => v.VideoUrl) ?? Enumerable.Empty<string>();
+
+            return await _lessonRepository.AddLesson(lessonDto.Title, lessonDto.Content, lessonDto.CourseId, pictureUrls, videoUrls);
         }
 
         public Lesson UpdateLesson(int id, string title, string content, int courseId, IEnumerable<string> pictureUrls, IEnumerable<string> videoUrls)
