80df3aea65055ba37fdd00d94cce86f23d14abc9
Hozaneybo <maheyb01@easv365.dk>
2023-12-21 04:58:19 +0100

    secure the CROS

--- Api/Program.cs
+++ Api/Program.cs
@@ -52,21 +52,21 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-/*app.UseCors(options =>
+app.UseCors(options =>
 { 
     options.WithOrigins("https://learning-platform-80506.web.app")
         .AllowAnyMethod()
         .AllowAnyHeader()
         .AllowCredentials();
-});*/
+});
 
-app.UseCors(options =>
+/*app.UseCors(options =>
 { 
     options.SetIsOriginAllowed(origin => true)
         .AllowAnyMethod()
         .AllowAnyHeader()
         .AllowCredentials();
-});
+});*/
 
 app.UseSecurityHeaders();
 app.UseSession();
